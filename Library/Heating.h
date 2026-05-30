cc1e569b0e335bcfbf1f37b94cb3a46b4296df16
CaoBX <caobx@example.com>
2026-05-30 11:13:22 +0800

    回滚和分支测试1

--- Library/Heating.h
+++ Library/Heating.h
@@ -1,9 +1,11 @@
 #ifndef __HEATING_H
 #define __HEATING_H
 
-void Heating_Set(float temperature);
-void Heating_ON(void);
-void Heating_OFF(void);
-void Heating_Control(float current_temperature);
+//回滚测试1
+
+void Heating_Set (float temperature);
+void Heating_ON (void);
+void Heating_OFF (void);
+void Heating_Control (float current_temperature);
 
 #endif /* __HEATING_H */
